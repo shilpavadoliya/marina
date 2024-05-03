83abdad43ed33e486414c280918b9ddb4bc93aa9
shilpavadoliya <web91.mihir@gmail.com>
2024-05-03 22:41:24 +0530

    order module

--- resources/pos/src/shared/prepareArray/preparePurchaseArray.js
+++ resources/pos/src/shared/prepareArray/preparePurchaseArray.js
@@ -1,33 +1,52 @@
 export const preparePurchaseProductArray = (products, isBarcode) => {
     let purchaseProductRowArray = [];
     products.forEach(product => {
+        const { 
+            attributes: { 
+                name, 
+                code, 
+                barcode_url, 
+                stock, 
+                product_unit, 
+                prices, 
+                minimum_price, 
+                product_cost, 
+                tax_type, 
+                order_tax, 
+                purchase_unit, 
+                product_unit_quantity,
+                product_price
+            }, 
+            id 
+        } = product;
+
+        const price = prices && prices.length > 0 ? parseFloat(prices[0].price) : parseFloat(product_price);
+        
         purchaseProductRowArray.push({
-            name: product.attributes.name,
-            code: product.attributes.code,
-            barcode_url: product.attributes.barcode_url,
-            stock: product.attributes.stock ? product.attributes.stock.quantity : "",
-            // short_name: product.attributes.purchase_unit_name.short_name,
-            product_unit: product.attributes.product_unit,
-            product_id: product.id,
-            product_cost: parseFloat(product.attributes.prices[0].price),
-            net_unit_cost: parseFloat(product.attributes.prices[0].price),
-            minimum_price: parseFloat(product.attributes.minimum_price),
-            fix_net_unit: product.attributes.product_cost,
-            tax_type: product.attributes.tax_type ? product.attributes.tax_type : 1,
-            tax_value: product.attributes.order_tax ? product.attributes.order_tax : 0.00,
+            name,
+            code,
+            barcode_url,
+            stock: stock ? stock.quantity : "",
+            product_unit,
+            product_id: id,
+            product_cost: price ? price : 0,
+            net_unit_cost: price ? price : 0,
+            minimum_price: (minimum_price) ? parseFloat(minimum_price) : 0,
+            fix_net_unit: product_cost,
+            tax_type: tax_type || 1,
+            tax_value: order_tax || 0.00,
             tax_amount: 0.00,
             discount_type: '2',
             discount_value: 0.00,
             discount_amount: 0.00,
-            purchase_unit: product.attributes.purchase_unit,
-            quantity: product.attributes.product_unit_quantity,
+            purchase_unit,
+            quantity: 1,
             sub_total: 0.00,
-            id: product.id,
+            id,
             purchase_item_id: '',
-            product_price: product.attributes.product_price
-        })
+            product_price
+        });
     });
-    console.log(products);
-    console.log('prepare');
+
     return purchaseProductRowArray;
 };
